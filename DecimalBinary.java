137101cf9273ecf6610dfed5b71f75017250bbd8
Disappointed-Asian <zeiusaben1@gmail.com>
2025-09-08 12:23:07 +0800

    Update DecimalBinary.java

--- DecimalBinary.java
+++ DecimalBinary.java
@@ -7,17 +7,18 @@ public class DecimalBinary{
 		
 		while (true){
 			//Ask for user input
-			System.out.println("Write a number:");
-			String decimal = scanner.nextLine();
+        	System.out.println("Type a number:");
+        	String decimal = scanner.nextLine();
 
-			//Exits loop after typing STOP
-			if (decimal.equalsIgnoreCase("STOP")) {
-				break;
+        	//Exits loop after typing STOP
+        	if (decimal.equalsIgnoreCase("STOP")) {
+        		break;
 			}
-
+                
 			//converts string to int
-			int input = Integer.parseInt(decimal);
-			String binary = "";
+        	try {
+				int input = Integer.parseInt(decimal);
+        		String binary = "";
 
 			//converts decimal to binary
 			while (input>0) {
@@ -28,12 +29,17 @@ public class DecimalBinary{
 				else if(input%2 ==1) {
 					binary = "1" + binary;
 					input = input/2;
+                    	}
 				}
-			}
 
 			//Prints the result
 			System.out.println("Input: " + decimal);
 			System.out.println("Output: " + binary);
+        	}
+        	catch(NumberFormatException e){
+        	System.out.println(ColoredText.GREEN + "Di ka lang mahal! Aray ko!");
+        	}
+		
 		}
 		scanner.close(); 
 	}
